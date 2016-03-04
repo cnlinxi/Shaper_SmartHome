c63b39d72810926a82b9d66a26f14c5bbd20dc8a
陈梦楠 <cncmn@hotmail.com>
2016-03-05 00:00:01 +0800

    2016年3月4日23:59:59

--- WebApiSample/Views/Page2.xaml.cs
+++ WebApiSample/Views/Page2.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WebApiSample.Common;
 using WebApiSample.Helpers;
 using WebApiSample.Model;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 namespace WebApiSample.Views
 {
@@ -14,15 +16,27 @@ namespace WebApiSample.Views
     {
         string userName = string.Empty;
         const string CommandTarget = "ShaperloT";
-        const string CommandHost = "http://mywebapidemo.azurewebsites.net";
+
+        const string CommandHost = "http://mywebapidemo.azurewebsites.net/api/Command";
+        const string EmergenceCounterHost = "http://mywebapidemo.azurewebsites.net/api/EmergenceCounter";
+
         const string CmdOn = "On";
         const string CmdOff = "Off";
+
+        List<EmergenceCounterInfo> lstEmergenceCounter;
         public Page2()
         {
             this.InitializeComponent();
 
             UserAccountService userAccount = new UserAccountService();
             userName = userAccount.GetUserNameFromLocker();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            lstEmergenceCounter = await GetEmergenceCounter();//获取到listEmergenceCounter,需要转化再绑定
+
+            base.OnNavigatedTo(e);
         }
 
         private async void toggleLed_Toggled(object sender, Windows.UI.Xaml.RoutedEventArgs e)
@@ -46,8 +60,7 @@ namespace WebApiSample.Views
         {
             if(commandContent!=string.Empty&&userName!=string.Empty)
             {
-                string queryString =
-                    string.Format("/api/Command?userName={0}&commandTarget={1}&commandContent={2}",
+                string queryString =string.Format("?userName={0}&commandTarget={1}&commandContent={2}",
                     userName, CommandTarget, commandContent);
                 string url = CommandHost + queryString;
                 HttpService http = new HttpService();
@@ -58,6 +71,20 @@ namespace WebApiSample.Views
         private void AppBarButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
 
+        }
+
+        private async Task<List<EmergenceCounterInfo>> GetEmergenceCounter()
+        {
+            if(userName.Length>0)
+            {
+                string queryString = string.Format("?userName={0}", userName);
+                HttpService http = new HttpService();
+                string strResponse = await http.SendGetRequest(EmergenceCounterHost + queryString);
+                List<EmergenceCounterInfo> lstEmergenceCounter = new List<EmergenceCounterInfo>();
+                JsonHelper.JsonToObject(strResponse, lstEmergenceCounter);
+                return lstEmergenceCounter;
+            }
+            return null;
         }
     }
 }
